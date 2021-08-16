49db3f476be909f0c9a41325c9d448edbea0d635
briPod <33463220+br1Pod@users.noreply.github.com>
2021-08-16 15:15:53 +0100

    class models added

---
+++ models/munro.py
@@ -0,0 +1,8 @@
+class Munro:
+
+    def __init__(self, name, height, climbed, region_id, id = None):
+        self.name = name
+        self.height = height
+        self.climbed = climbed
+        self.region_id = region_id
+        self.id = id
