09db0bef237c60e34b1712b5b23f0853e980436a
briPod <33463220+br1Pod@users.noreply.github.com>
2021-08-16 13:57:54 +0100

    refactored the database relationship - removed users (as only 1 - logged in), and removed visits table. Compleatr.sql file updated accordingly

---
+++ db/compleatr.sql
@@ -0,0 +1,17 @@
+
+DROP TABLE regions;
+DROP TABLE munros;
+
+
+CREATE TABLE regions (
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(255),   
+);
+
+CREATE TABLE munros (
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(255),
+    height VARCHAR(255),
+    climbed BOOLEAN,
+    regions_id INT REFERENCES regions(id)
+);
