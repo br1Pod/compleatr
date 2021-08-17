c2777648d1299eece0b07bb11ecf8c3660241b90
briPod <33463220+br1Pod@users.noreply.github.com>
2021-08-17 11:00:18 +0100

    more sample objects inserted, 2nd level display pages added

--- console.py
+++ console.py
@@ -7,7 +7,17 @@ munro_repository.delete_all()
 region_repository.delete_all()
 
 region1 = Region("Torridon")
-munro1 = Munro("Liathach", "2000m", False, region1)
-
 region_repository.save(region1)
+munro1 = Munro("Liathach", "2000 ft", False, region1)
 munro_repository.save(munro1)
+
+region2 = Region("Cairngorm")
+region_repository.save(region2)
+munro2 = Munro("Braeriach", "2500 ft", False, region2)
+munro_repository.save(munro2)
+
+region3 = Region("Cuillin")
+region_repository.save(region3)
+munro3 = Munro("Sgùrr a Mhadaidh", "918 m", True, region3)
+munro_repository.save(munro3)
+
